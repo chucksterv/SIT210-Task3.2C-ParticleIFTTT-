e3319b0b9e77e69bc59005f57038602a1f2aee56
chucksterv <deshanvikum@gmail.com>
2020-06-05 20:58:42 +1000

    Add files via upload

---
+++ ifttt.ino
@@ -0,0 +1,32 @@
+int led = D7;
+int lightSensor = A1;
+int lightData = 0;
+
+void setup() {
+    
+
+    
+    pinMode(led, OUTPUT);
+    pinMode(lightSensor, INPUT);
+}
+
+void loop() {
+    
+  digitalWrite(led, HIGH);   // Turn ON the LED
+  lightData = analogRead (lightSensor);
+  
+  if (lightData >= 2500)
+  {
+      Particle.publish("Sunlight", "Hits");
+  }
+  else
+  {
+      Particle.publish("Sunlight", "Hits");
+  }
+  
+    
+
+  digitalWrite(led, LOW);    // Turn OFF the LED
+  delay(10000);               // Wait for 1 seconds
+
+}
